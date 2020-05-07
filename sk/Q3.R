da0cc2bafa6947ee0e8532e68606dadee75570c6
SK. Ng <seekiatng@gmail.com>
2020-05-07 15:05:28 +0800

    init folder and the R file.

---
+++ sk/Q3.R
@@ -0,0 +1,12 @@
+
+# check the working directory
+getwd()
+
+# read the file
+data <- read.csv("singapore-Airbnb.csv")
+
+# check the file 
+head(data)
+colnames(data)
+
+# Q3 : where is the most demanding area in Central Region? 
