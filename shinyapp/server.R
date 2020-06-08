28d6cceb1ec06e9a254b6b1c5f437f793d8ae347
SK. Ng <seekiatng@gmail.com>
2020-06-08 14:18:56 +0800

    submit shinyapp by Praba

---
+++ shinyapp/server.R
@@ -0,0 +1,54 @@
+shinyServer(function(input, output,session){
+  
+  observe({
+    selected1=unique(SGSIN[SGSIN$neighbourhood_group==input$selected0,'neighbourhood'])
+    updateSelectizeInput(
+      session,'selected1',
+      choices = selected1,
+      selected = selected1[1]
+    )
+  })
+  
+  
+  listingSGSIN <- reactive({
+    selected0 = input$selected0
+    selected1 = input$selected1
+    selected2 = input$selected2
+    SGSIN %>%
+      select(neighbourhood_group, neighbourhood,room_type, longitude,latitude,price) %>%
+      filter(neighbourhood_group == selected0 & neighbourhood == selected1 & room_type == selected2)
+  })  
+  output$map <- renderLeaflet({
+    leaflet()%>% setView(lng = 103.851959, lat = 1.290270, zoom = 11) %>% 
+      addTiles() %>% addMarkers(lng = listingSGSIN()$longitude, lat = listingSGSIN()$latitude)
+  })
+  
+  output$plotmap <- renderPlot({
+    ggplot(listingSGSINplot %>% filter(neighbourhood_group == input$selected0 & neighbourhood == input$selected1), aes(price, color=room_type)) + geom_freqpoly()
+  })
+  
+  listingSGSINplot <- SGSIN %>%
+    select(neighbourhood_group, neighbourhood, room_type, price, minimum_nights)
+  
+  output$plot1 <- renderPlot({
+    ggplot(listingSGSINplot %>% filter(minimum_nights >= input$selected4[1] & minimum_nights <= input$selected4[2] &
+                                       price >= input$selected3[1] & price <= input$selected3[2]), aes(neighbourhood_group)) + 
+      geom_bar(aes(fill=room_type), position = position_dodge())
+  })
+  
+  output$plot2 <- renderPlot({
+    # listingSGSINplot %>% group_by(neighbourhood_group) %>% su
+    ggplot(listingSGSINplot %>% filter(minimum_nights >= input$selected6[1] & minimum_nights <= input$selected6[2]),
+           aes(neighbourhood_group, price)) + 
+      geom_bar(aes(fill=room_type), position = position_dodge(),stat = "identity")
+  })
+  output$plot3 <- renderPlot({
+    ggplot(listingSGSINplot %>% filter(minimum_nights >= input$selected5[1] & minimum_nights <= input$selected5[2] & neighbourhood_group == input$selected7),
+           aes(neighbourhood, price)) + 
+      geom_bar(aes(fill=room_type), position = position_dodge(),stat = "identity") +
+      theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
+  })
+  output$table <- DT::renderDataTable({
+    datatable(SGSIN, rownames=FALSE)
+  })
+})
---
+++ shinyapp/ui.R
@@ -0,0 +1,111 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+library(shinythemes)
+shinyUI(dashboardPage(
+  dashboardHeader(title = "Airbnb"),
+  dashboardSidebar(
+    sidebarUserPanel("Airbnb Singapore", image = "https://fintechnews.sg/wp-content/uploads/2019/10/Smart-Cities-AI-Singapore-1440x564_c.jpg"),
+    sidebarMenu(
+      menuItem("Intro", tabName = "intro", icon = icon("image")),
+      menuItem("Map", tabName = "map", icon = icon("map")),
+      menuItem("Bar Plot", tabName = "plot1", icon = icon("chart-bar")),
+      menuItem("Price Chart", tabName = "price", icon = icon("chart-line")),
+      menuItem("Data", tabName = "data", icon = icon("database")),
+      menuItem("About Group", tabName = "Group", icon = icon("user-circle")))
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "intro",
+              HTML('<img src="https://fintechnews.sg/wp-content/uploads/2019/10/Smart-Cities-AI-Singapore-1440x564_c.jpg?auto=compress&cs=tinysrgb&h=750&w=1260" width=500 height=700>')
+      ),
+      
+      tabItem(tabName = "map",
+              fluidPage(
+                
+                box(
+                  
+                  title = "listing based on search",
+                  leafletOutput("map"),
+                  selectizeInput("selected0",
+                                 "Select Item to Display",
+                                 unique(SGSIN$neighbourhood_group)),
+                  
+                  selectizeInput("selected1",
+                                 "Select Item to Display",
+                                 unique(SGSIN$neighbourhood)),
+                  
+                  selectizeInput("selected2",
+                                 "Select Item to Display",
+                                 unique(SGSIN$room_type))),
+                box(
+                  title = "Frquency of room types based on Price",
+                  plotOutput("plotmap"),
+                  dataTableOutput('table1')
+                ))),
+      
+      tabItem(tabName = "price",
+              tabsetPanel(type= "tabs",
+                          tabPanel("Borough",
+                                   fluidRow(
+                                     plotOutput('plot2'),
+                                     box(
+                                       sliderInput("selected6",
+                                                   "Pick minimum nights",
+                                                   min = 1, max = 1250, value = c(1,1250))
+                                     )
+                                   )
+                          ),
+                          tabPanel("Neighborhood",
+                                   fluidRow(
+                                     plotOutput('plot3'),
+                                     box(
+                                       selectizeInput("selected7",
+                                                      "Select Borough",
+                                                      unique(SGSIN$neighbourhood_group)),
+                                       sliderInput("selected5",
+                                                   "Pick minimum nights",
+                                                   min = 1, max = 1250, value = c(1,1250))
+                                     )
+                                   ))
+                          
+              )),
+      
+      tabItem(tabName = "plot1",
+              fluidRow(
+                # box(
+                plotOutput('plot1'),
+                box(sliderInput("selected3",
+                                "Pick a price range", pre = "$",
+                                min=1, max= 10000, value = c(1, 10000)),
+                    sliderInput("selected4",
+                                "Pick minimum nights",
+                                min = 1, max = 1250, value = c(1,1250))
+                )
+                
+              )),
+      
+      tabItem(tabName = "data",
+              fluidPage(dataTableOutput('table')
+              )),
+      tabItem(tabName = "Group",
+              fluidPage(
+                h4("Prabavathi WQD 180030", br(),
+                   "ADEDIGBA STEPHEN OLAMILEKAN 17220137", br(),
+                    "Ali Alrabeei 17219152",
+                    "Kaveenaasvini WQD 180017",br(),
+                    "Ng see kiat 17219523", br(),
+                    "Kumarmangal Roy 17220389", br()),
+                h4(a(icon= icon("github"),"Github Link", href="https://github.com/kroy69/Factors-that-affected-the-airbnb-price-in-Singapore/blob/master/Group2.R"))
+                
+              )
+              
+              
+              
+      )
+    ))))
